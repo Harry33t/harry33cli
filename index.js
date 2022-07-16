45b8f221ecd0fd5b9593f6f9f1cf51860b29fa77
qq13621887478 <1090783572@qq.com>
2022-07-16 19:22:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node  
+
+const program = require('commander')
+const {helpOptions} = require('./lib/core/help')
+const createCommands = require('./lib/core/create')
+//查看版本号
+program.version(require('./package.json').version);
+//帮助信息
+helpOptions()
+//创建信息
+createCommands()
+
+
+program.parse(process.argv)
+ 
+
